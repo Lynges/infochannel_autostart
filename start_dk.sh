849e1d81ee4981e0b60ee65e2c65b9ad9d3a4a8c
Mathias Nielsen <lynge@lynges.net>
2018-06-19 22:15:02 +0200

    grrrr

--- start_dk.sh
+++ start_dk.sh
@@ -3,4 +3,4 @@ wget https://raw.githubusercontent.com/Lynges/infochannel_autostart/master/autoi
 chmod +x autoinstall.sh
 ./autoinstall.sh
 
-sed -i 's;http://localhost:8080;https://docs.google.com/presentation/d/e/2PACX-1vTgsLkuhnHVGmVA2VAbREN4AjMAHbMppX1f6Gr4WauBAu9s8IyNzFKra5PnYb9YWo2aLZD8gdivhSrR/pub?start=true\\\&loop=true\\\&delayms=7000;g' /root/.xsession
+sed -i 's;http://localhost:8080;https://docs.google.com/presentation/d/e/2PACX-1vTgsLkuhnHVGmVA2VAbREN4AjMAHbMppX1f6Gr4WauBAu9s8IyNzFKra5PnYb9YWo2aLZD8gdivhSrR/pub?start=true\\\&loop=true\\\&delayms=7000;g' /home/dietpi/.xsession
--- start_en.sh
+++ start_en.sh
@@ -3,4 +3,4 @@ wget https://raw.githubusercontent.com/Lynges/infochannel_autostart/master/autoi
 chmod +x autoinstall.sh
 ./autoinstall.sh
 
-sed -i 's;http://localhost:8080;https://docs.google.com/presentation/d/e/2PACX-1vRcxD0NpEuCX0fvUYnnrDkLV9DIL43PEv_KSrA59douBSWgnxP69xR4ON-Ft4loWpDDSd8Ht0bma811/pub?start=true\\\&loop=true\\\&delayms=7000;g' /root/.xsession
+sed -i 's;http://localhost:8080;https://docs.google.com/presentation/d/e/2PACX-1vRcxD0NpEuCX0fvUYnnrDkLV9DIL43PEv_KSrA59douBSWgnxP69xR4ON-Ft4loWpDDSd8Ht0bma811/pub?start=true\\\&loop=true\\\&delayms=7000;g' /home/dietpi/.xsession
